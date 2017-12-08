741fad631fc490aaf2c8b279d7f5d0e897196855
Yuen, Geoff <gyuen@lcs-mba3098.lcs.on.ca>
2017-12-08 10:41:51 -0500

    not loading

--- 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/1 - Introduction to Swift and Playgrounds/lab/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -1,15 +1,16 @@
-/*: 
+/*:
  ## Exercise - Use Playgrounds
  
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
-print("I have learned the following:")
-print("What features make Swift a modern and safe language")
-print("How to use the Swift REPL in Terminal")
-print("How to use playgrounds to make writing Swift fun and simple")
+
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
+print("Somebody once told me the world was gonna roll me")
+print("I ain't the sharpest tool in the shed")
+print("She was looking kind of dumb with her finger and her thumb")
+print("In the shape of an L on her forehead")
 
 
 /*:
--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -3,7 +3,8 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
+let friends = 0
+print = friends
 
 /*:
  Now assume you go through and remove friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is.
